ba5dfd2df20af7fbf741347b57cb24550782e8db
Luke <lgrilli03@gmail.com>
2024-02-14 20:10:14 -0500

    folder changes

--- main.c
+++ main.c
@@ -4,4 +4,6 @@ int CALLBACK WinMain(HINSTANCE hInst, HINSTANCE xHinst, LPSTR lpstr, int cmdShow
     MainWindow mainWindow;
     InitWindow(&mainWindow, "green", 600, 600);
     StartWindow(&mainWindow, hInst, cmdShow);
+
+    return 0;
 }
